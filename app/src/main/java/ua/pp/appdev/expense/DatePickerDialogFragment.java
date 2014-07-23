4fdfc1ef5d7328517b1a43b3828bcf6244d47398
s0ph1e <sonya1912@gmail.com>
2014-07-23 16:40:31 +0300

    add datetimepicked fragment dialog

--- app/src/main/java/ua/pp/appdev/expense/DatePickerDialogFragment.java
+++ app/src/main/java/ua/pp/appdev/expense/DatePickerDialogFragment.java
@@ -1,21 +1,42 @@
 package ua.pp.appdev.expense;
 
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.app.FragmentTransaction;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.Interpolator;
+import android.view.animation.TranslateAnimation;
 import android.widget.DatePicker;
+import android.widget.LinearLayout;
+import android.widget.TimePicker;
 
 import java.util.Calendar;
 
-public class DatePickerDialogFragment extends DialogFragment
-        implements DatePickerDialog.OnDateSetListener {
+public class DatePickerDialogFragment extends DialogFragment implements View.OnClickListener{
+
+    private View view;
+    private DatePicker datePicker;
+    private TimePicker timePicker;
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the current date as the default date in the picker
-        final Calendar c = Calendar.getInstance();
+        /*final Calendar c = Calendar.getInstance();
         int year = c.get(Calendar.YEAR);
         int month = c.get(Calendar.MONTH);
         int day = c.get(Calendar.DAY_OF_MONTH);
@@ -23,10 +44,80 @@ public class DatePickerDialogFragment extends DialogFragment
         // Create a new instance of DatePickerDialog and return it
         DatePickerDialog dialog = new DatePickerDialog(getActivity(), this, year, month, day);
         dialog.getDatePicker().setCalendarViewShown(false);
-        return dialog;
+        return dialog;*/
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        AlertDialog.Builder adb = new AlertDialog.Builder(getActivity())
+                .setTitle("Set date and time")
+                .setPositiveButton(R.string.done, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+
+                    }
+                });
+
+        view = getActivity().getLayoutInflater().inflate(R.layout.date_time_picker_layout, null);
+
+        datePicker = (DatePicker) view.findViewById(R.id.datePicker);
+        timePicker = (TimePicker) view.findViewById(R.id.timePicker);
+        timePicker.setIs24HourView(true);
+
+        view.findViewById(R.id.btnSetTime).setOnClickListener(this);
+
+        view.findViewById(R.id.btnSetDate).setOnClickListener(this);
+
+        adb.setView(view);
+
+        return adb.create();
     }
 
-    public void onDateSet(DatePicker view, int year, int month, int day) {
-        // Do something with the date chosen by the user
+    @Override
+    public void onClick(View view) {
+        View dateView = this.view.findViewById(R.id.layoutDatePicker);
+        View timeView = this.view.findViewById(R.id.layoutTimePicker);
+
+        switch (view.getId()){
+            case R.id.btnSetTime:
+            case R.id.btnSetDate:
+                flip(dateView, timeView);
+                break;
+        }
+    }
+
+    private Interpolator accelerator = new AccelerateInterpolator();
+    private Interpolator decelerator = new DecelerateInterpolator();
+    private void flip(View a, View b) {
+        final View visibleList;
+        final View invisibleList;
+        if (a.getVisibility() == View.GONE) {
+            visibleList = b;
+            invisibleList = a;
+        } else {
+            invisibleList = b;
+            visibleList = a;
+        }
+        ObjectAnimator visToInvis = ObjectAnimator.ofFloat(visibleList, "rotationY", 0f, 90f);
+        visToInvis.setDuration(500);
+        visToInvis.setInterpolator(accelerator);
+        final ObjectAnimator invisToVis = ObjectAnimator.ofFloat(invisibleList, "rotationY",
+                -90f, 0f);
+        invisToVis.setDuration(500);
+        invisToVis.setInterpolator(decelerator);
+        visToInvis.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator anim) {
+                visibleList.setVisibility(View.GONE);
+                invisToVis.start();
+                invisibleList.setVisibility(View.VISIBLE);
+            }
+        });
+        visToInvis.start();
     }
 }
