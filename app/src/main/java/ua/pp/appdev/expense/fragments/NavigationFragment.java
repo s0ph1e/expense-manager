0d213e89bccab37a61f695b86b4ebff5a17c1f77
s0ph1e <sonya1912@gmail.com>
2014-07-27 17:57:25 +0300

    fix on onOptionsItemSelected in navigator

--- app/src/main/java/ua/pp/appdev/expense/fragments/NavigationFragment.java
+++ app/src/main/java/ua/pp/appdev/expense/fragments/NavigationFragment.java
@@ -187,7 +187,7 @@ public class NavigationFragment extends Fragment {
     public boolean onOptionsItemSelected(MenuItem item) {
         // Pass the event to ActionBarDrawerToggle, if it returns
         // true, then it has handled the app icon touch event
-        if (mDrawerToggle.onOptionsItemSelected(item)) {
+        if (mDrawerLayout != null && mDrawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
         // Handle your other action bar items...
