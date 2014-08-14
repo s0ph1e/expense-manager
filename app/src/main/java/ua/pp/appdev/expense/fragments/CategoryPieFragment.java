7e3014dd09c6c77c19c975e2193b77d8ae4a6008
s0ph1e <sonya1912@gmail.com>
2014-08-14 17:42:08 +0300

    fix first category selected on category click when adapter contains 1 item

--- app/src/main/java/ua/pp/appdev/expense/fragments/CategoryPieFragment.java
+++ app/src/main/java/ua/pp/appdev/expense/fragments/CategoryPieFragment.java
@@ -81,7 +81,9 @@ public class CategoryPieFragment extends Fragment {
         categoriesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                setSelected(i);
+                if(categoriesAdapter.getCount() > 1) {
+                    setSelected(i);
+                }
             }
         });
 
